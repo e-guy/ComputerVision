9061c7a187f4d8b8f3fcb757ff2b9d2829854044
e-guy <e-guy@foxmail.com>
2018-03-08 16:02:44 +1100

    Add files via upload

---
+++ lab1/opencv.py
@@ -0,0 +1,17 @@
+'''
+    This is a Python script try to load and display images by using opencv 
+    (Source images can be found under the folder './DataSamples/')
+    (Useful tutorials refers to: https://opencv24-python-tutorials.readthedocs.io/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html#display-image)
+'''
+
+#!/usr/bin/python
+import cv2
+
+# Load an color image in grayscale
+img1 = cv2.imread('DataSamples/2.png',0) # must be in full path name
+
+# Display the image
+cv2.imshow('image1',img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
